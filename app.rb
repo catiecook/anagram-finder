87cdbc47fdeae422eed01e03fbe8411fb3d428f5
catiecook <catiecook888@gmail.com>
2017-03-02 18:15:37 -0700

    json data

--- app.rb
+++ app.rb
@@ -4,36 +4,24 @@ require 'json'
 class App < Sinatra::Base
   # open the text file
   get '/' do
-    # in here a render of the index page
+    #in here a render of the index page
   end
 
   # This is a sample dynamic route.
   get "/search/:word" do
+
     @word = params[:word]
     word = @word
     arrange_word(word)
-    "#{@word}"
-    render json: @anagrams
+    # "The Word was: #{@word}"
+    content_type :json
+    @anagrams.to_json
   end
 
   # **** actions *****
   def index
     @greeting = "Whats The Word?"
   end
-
-
-  # def search
-  #   puts "in search route"
-  #   get_word
-  #   # renders word's anagrams to json on /search
-  #   render json: @anagrams
-  # end
-
-  # def get_word
-  #   # get word from form
-  #   base_word = params['base_word']
-  #   arrange_word(base_word) #function that sorts word to find anagram
-  # end
 
   def arrange_word(base_word)
     # remove white space
